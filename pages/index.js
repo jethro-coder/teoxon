158adde3438da4d36aa78813a55dc7d33821d2c5
Jethro-Coder <jethrovillarosa123@gmail.com>
2025-09-12 20:29:35 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Hero from "./Hero";
+import Content from "./Content";
+import Contact from "./Contact";
+import Blog from "./Blog";
 
 export default function Home() {
   return (
     <div>
-      Jethro V. Teoxon
+      <Hero />
+      <Content />
+      <Blog />
+      <Contact />
     </div>
   );
 }
