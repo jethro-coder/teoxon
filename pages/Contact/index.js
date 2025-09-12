91de6ff6417f58e09a35f9e348cc9152a2982001
Jethro-Coder <jethrovillarosa123@gmail.com>
2025-09-12 20:49:28 +0800

    Commit 2

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -8,7 +8,7 @@ function index() {
   </div>
   <div class="container px-5 py-24 mx-auto flex">
     <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">JASDJADJKAJDAKDJAKDAJDAKDJAKDSJ</h2>
       <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
       <div class="relative mb-4">
         <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
