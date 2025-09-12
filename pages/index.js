b3c27d67aba871048bce980e6a3d6d80ea7f6f04
Jethro-Coder <jethrovillarosa123@gmail.com>
2025-09-12 13:07:43 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div>
+      Jethro V. Teoxon
+    </div>
+  );
+}
